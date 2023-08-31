212693a3884bb774cc6a1505412e5604e7280fd8
Ryan Donald <ryanthomasdonald@gmail.com>
2023-08-31 11:41:58 -0400

    Thurs. lunch

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
+import { Link } from 'react-router';
 
 class SongList extends Component {
 
@@ -8,7 +9,7 @@ class SongList extends Component {
         return this.props.data.songs.map(song => {
             return (
                 <li key={song.id} className="collection-item">
-                    {song.title};
+                    {song.title}
                 </li>
             );
         });
@@ -20,9 +21,17 @@ class SongList extends Component {
         };
 
         return (
-            <ul className="collection">
-                {this.renderSongs()};
-            </ul>
+            <div>
+                <ul className="collection">
+                    {this.renderSongs()}
+                </ul>
+                <Link
+                    to="/songs/new"
+                    className="btn-floating btn-large red right"
+                >
+                    <i className="material-icons">add</i>
+                </Link>
+            </div>
         );
     };
 };
