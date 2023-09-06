e332d79e2e2a731997c303ac1443b5bb7f7bed72
Ryan Donald <ryanthomasdonald@gmail.com>
2023-09-06 10:46:41 -0400

    Thurs WFH push

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -14,12 +14,13 @@ class SongList extends Component {
         return this.props.data.songs.map(({ id, title }) => {
             return (
                 <li key={id} className="collection-item">
-                    {title}
+                    <Link to={`/songs/${id}`}>{title}</Link>
                     <i
                         className="material-icons"
                         onClick={() => this.onSongDelete(id)}
                     >
-                        delete</i>
+                        delete
+                    </i>
                 </li>
             );
         });
